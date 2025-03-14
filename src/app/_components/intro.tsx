76b8531b3ac48b173fc7fa522716bf538c07251b
BEW111 <bewilliams111@gmail.com>
2025-03-13 20:12:13 -0400

    description

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,11 +1,11 @@
 export function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+    <section className="flex-col flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <h1 className="text-4xl sm:text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
         brian edward williams
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        description
+      <h4 className="text-center text-lg sm:text-2xl md:text-3xl">
+        software and ai
       </h4>
     </section>
   );
